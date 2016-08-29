9ad53766747d3b227f1fb2e0424a675e79b68be5
Albert Martin <albert@imor.tl>
2016-08-29 16:05:50 -0500

    chore(leadForm): autofocus field after modal has opened

--- frontend/components/app/index.js
+++ frontend/components/app/index.js
@@ -1,4 +1,15 @@
 angular.module('TreeHouse.component.app', [])
 .component('app', {
-  template: require('./template.html')
+  template: require('./template.html'),
+  controller: ['$timeout', function($timeout) {
+    this.showPrice = false;
+
+    this.togglePrice = function() {
+      this.showPrice = !this.showPrice;
+      $timeout(() => {
+        var firstName = document.getElementById('first_name');
+        firstName[this.showPrice ? 'focus' : 'blur']();
+      }, 400);
+    }
+  }]
 });
